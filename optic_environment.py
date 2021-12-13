71806cafc27239a102226b41469c9624821a354d
dvirmarsh <dvirmarsh>
2021-12-13 15:14:32 +0200

    added circular env

--- optic_environment.py
+++ optic_environment.py
@@ -7,6 +7,10 @@ import torch.nn as nn
 import random
 from tqdm import tqdm
 import cv2
+import matplotlib.pyplot as plt
+from scipy import ndimage
+from stable_baselines3 import TD3
+from stable_baselines3.common.evaluation import evaluate_policy
 
 class optic_propagation():
     def __init__(self,SLM,
@@ -23,9 +27,11 @@ class optic_propagation():
         self.tishue_focus_point = distance_tishue_focus_point
         self.wave_length = wave_length
         self.scale = scale
-        self.field = laser_in_field
-        factor = laser_in_field.shape[0]/SLM.shape[0]
-        self.SLM = np.repeat(np.repeat(SLM, factor, axis=1), factor, axis=0)
+        self.field = np.complex128(laser_in_field)
+        #factor = laser_in_field.shape[0]/SLM.shape[0]
+        #self.SLM = np.repeat(np.repeat(SLM, factor, axis=1), factor, axis=0)
+        N = (laser_in_field.shape[0]-SLM.shape[0])//2
+        self.SLM = np.pad(SLM, N, mode='edge')
 
     "scale operator" \
     "input:a-scale factor"
@@ -61,11 +67,14 @@ class optic_propagation():
     "propagation with phase from the SLM and tishue phase" \
     "outputs: the intensity taken by the camera"
     def diverge_and_scatter(self):
-        self.R_diverges(self.laser_SLM)
-        self.field *= self.SLM
-        self.R_diverges(self.SLM_tishue)
-        self.field *= self.Tishue_phase
-        self.R_diverges(self.tishue_focus_point)
+        if self.laser_SLM>0:
+            self.R_diverges(self.laser_SLM)
+        self.field *= np.exp(1j*self.SLM)
+        if self.SLM_tishue>0:
+            self.R_diverges(self.SLM_tishue)
+        self.field *= np.exp(1j*self.Tishue_phase)
+        if self.tishue_focus_point>0:
+            self.R_diverges(self.tishue_focus_point)
         return np.abs(self.field)**2
 
 
@@ -78,7 +87,8 @@ class optic_env(Env):
                  distance_laser_SLM,
                  distance_SLM_tishue,
                  distance_tishue_focus_point,
-                 laser_beam_radius
+                 laser_beam_radius,
+                 resize=2
                  ):
         self.SLM_size = SLM_size
         self.pic_size = pic_size
@@ -88,19 +98,20 @@ class optic_env(Env):
         self.tishue_focus_point = distance_tishue_focus_point
         self.scale = np.linspace(-input_length/2, input_length/2, pic_size[0])
         X, Y = np.meshgrid(self.scale, self.scale)
-        self.laser_in_field = np.float(X**2 + Y**2 <= laser_beam_radius**2)
+        self.laser_in_field = np.float32(X**2 + Y**2 <= laser_beam_radius**2)
         N = pic_size[0]
-        self.row_idx = [N/2-1, N/2-1, N/2, N/2]
-        self.col_idx = [N/2-1, N/2, N/2-1, N/2]
-        self.action_space = Box(low=-np.pi, high=np.pi, shape=SLM_size)
-        self.observation_space = Box(low=0, high=1, shape=pic_size)
+        self.row_idx = [int(N/2-1), int(N/2-1), int(N/2), int(N/2)]
+        self.col_idx = [int(N/2-1), int(N/2), int(N/2-1), int(N/2)]
+        self.action_space = Box(low=-np.pi, high=np.pi, shape=(np.prod(SLM_size),))
+        self.observation_space = Box(low=0, high=255, shape=(*pic_size, 1), dtype=np.uint8)
+        self.resize = resize
 
 
 
 
     def reset(self):
-        self.SLM = np.ones(self.SLM_size)
-        self.tishue_phase = (np.random.rand(self.pic_size) -1/2)*2*np.pi
+        self.SLM = np.zeros((self.SLM_size[0]*self.resize, self.SLM_size[0]*self.resize))
+        self.tishue_phase = (np.random.rand(*self.pic_size) -1/2)*2*np.pi
         prop = optic_propagation(self.SLM,
                                  self.tishue_phase,
                                  self.laser_SLM,
@@ -109,11 +120,18 @@ class optic_env(Env):
                                  self.wave_length,
                                  self.scale,
                                  self.laser_in_field)
-        return prop.diverge_and_scatter()
+
+        obs = prop.diverge_and_scatter()
+        obs = np.uint8((obs - np.min(obs)) / (np.max(obs) - np.min(obs)) * 255)
+        obs = np.expand_dims(obs, axis=-1)
+        self.counter = 0
+        return obs
 
     def step(self, delta_phase):
         self.update_tishue_phase()
-        self.SLM *= np.exp(1j*delta_phase)
+        delta_phase = np.reshape(delta_phase, self.SLM_size)
+        delta_phase = cv2.resize(delta_phase, (self.SLM_size[0]*self.resize, self.SLM_size[1]*self.resize), interpolation=cv2.INTER_CUBIC)
+        self.SLM += delta_phase
         prop = optic_propagation(self.SLM,
                                  self.tishue_phase,
                                  self.laser_SLM,
@@ -123,14 +141,151 @@ class optic_env(Env):
                                  self.scale,
                                  self.laser_in_field)
         obs = prop.diverge_and_scatter()
-        reward = np.mean(obs[self.row_idx, self.col_idx])
+        obs = np.uint8((obs-np.min(obs))/(np.max(obs)-np.min(obs))*255)
+        reward = np.min(obs[self.row_idx, self.col_idx])/np.mean(obs)
+        obs = np.expand_dims(obs, axis=-1)
+        self.counter += 1
+        done = False
+        if self.counter == 100:
+            done = True
+        info = {}
+        return obs, reward, done, info
+
+    def update_tishue_phase(self):
+        return
+
+class CircEnv(optic_env):
+    def __init__(self, SLM_size,
+                 pic_size,
+                 input_length,
+                 wave_length,
+                 distance_laser_SLM,
+                 distance_SLM_tishue,
+                 distance_tishue_focus_point,
+                 laser_beam_radius,
+                 d_theta,
+                 resize=2
+                 ):
+        super(CircEnv, self).__init__(SLM_size,
+                                      pic_size,
+                                      input_length,
+                                      wave_length,
+                                      distance_laser_SLM,
+                                      distance_SLM_tishue,
+                                      distance_tishue_focus_point,
+                                      laser_beam_radius,
+                                      resize)
+        self.d_theta = d_theta
+        self.N = self.pic_size[0]
+
+    def update_tishue_phase(self):
+        Tishue = ndimage.rotate(self.Big_Tishue, self.d_theta*self.counter, reshape=False, order=5)
+        self.tishue_phase = Tishue[self.N//2:-self.N//2, self.N//2:-self.N//2] + np.random.randn(*self.pic_size)*0.1
+
+    def reset(self):
+        self.SLM = np.zeros((self.SLM_size[0]*self.resize, self.SLM_size[0]*self.resize))
+        self.Big_Tishue = (np.random.rand(self.N*2, self.N*2) -1/2)*2*np.pi
+        self.tishue_phase = self.Big_Tishue[self.N//2:-self.N//2,self.N//2:-self.N//2]
+        prop = optic_propagation(self.SLM,
+                                 self.tishue_phase,
+                                 self.laser_SLM,
+                                 self.SLM_tishue,
+                                 self.tishue_focus_point,
+                                 self.wave_length,
+                                 self.scale,
+                                 self.laser_in_field)
+
+        obs = prop.diverge_and_scatter()
+        obs = np.uint8((obs - np.min(obs)) / (np.max(obs) - np.min(obs)) * 255)
+        obs = np.expand_dims(obs, axis=-1)
+        self.counter = 0
+        return obs
 
 
-        return obs, reward, 0
 
 
 
 
+
+
+
+
+
+env = CircEnv((4,4),
+              (128,128),
+              2e-2,
+              532e-9,
+              0,
+              4e-2,
+              4e-2,
+              1e-3,
+              1)
+model = TD3("CnnPolicy", env, buffer_size = int(1e3),gamma=0.99)
+
+done = False
+obs = env.reset()
+obs0 = obs
+rewards = []
+action = np.zeros((16,))
+while done!=True:
+    #action, _ = model.predict(obs)
+    obs, reward, done, _ = env.step(action)
+    rewards.append(reward)
+
+imgplot = plt.imshow(obs)
+plt.show()
+eval_env = CircEnv((4,4),
+                   (128,128),
+                   2e-2,
+                   532e-9,
+                   0,
+                   4e-2,
+                   4e-2,
+                   1e-3,
+                   1)
+mean_reward, std_reward = evaluate_policy(model, eval_env, n_eval_episodes=10, deterministic=True)
+print(f"mean_reward={mean_reward:.2f} +/- {std_reward}")
+
+model.learn(total_timesteps=int(1e6))
+
+mean_reward, std_reward = evaluate_policy(model, eval_env, n_eval_episodes=10, deterministic=True)
+print(f"mean_reward={mean_reward:.2f} +/- {std_reward}")
+
+done = False
+obs = env.reset()
+obs0 = obs
+rewards = []
+while done!=True:
+    action, _ = model.predict(obs)
+    obs, reward, done, _ = env.step(action)
+    rewards.append(reward)
+
+imgplot = plt.imshow(obs)
+plt.show()
+a = 0
+
+
+
+
+"""wave_length = 633e-9
+N = 1024
+scale = np.linspace(-0.005,0.005,N)
+
+[X,Y] = np.meshgrid(scale,scale)
+laser_in_field = np.float32(((X-0.6/N)**2+(Y-0.6/N)**2<=1/N**2)&((X-0.6/N)**2+(Y-0.6/N)**2>=0.25/N**2))
+SLM = np.zeros((N,N))
+Tishue_phase = np.zeros((N,N))
+distance_laser_SLM = 0
+distance_SLM_tishue = 0.5
+distance_tishue_focus_point = 0
+circ = optic_propagation(SLM, Tishue_phase, distance_laser_SLM, distance_SLM_tishue, distance_tishue_focus_point, wave_length, scale, laser_in_field)
+neg_circ = optic_propagation(SLM, Tishue_phase, distance_laser_SLM, distance_SLM_tishue, distance_tishue_focus_point, wave_length, scale, 1-laser_in_field)
+im = circ.diverge_and_scatter()
+im = circ.diverge_and_scatter()
+imgplot = plt.imshow(im)
+plt.show()"""
+
+a=0
 
 
 
